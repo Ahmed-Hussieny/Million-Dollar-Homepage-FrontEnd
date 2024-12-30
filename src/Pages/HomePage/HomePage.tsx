546200e1e90aea5e48cf050f1ef7a9e64c586969
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-12-30 12:59:32 +0200

    toast, env & small Documentation

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -2,41 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { useAppDispatch } from '../../Store/store';
 import { setLoading } from '../../Store/globalSlice';
 import { getLogos } from '../../Store/LogosSlices';
+import axios from 'axios';
 
 const HomePage: React.FC = () => {
   const [svgContent, setSvgContent] = useState<string | null>(null);  // To hold the SVG content as a string
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(setLoading(true));
-    dispatch(getLogos()).unwrap();
-    fetch('https://2d15-102-46-146-22.ngrok-free.app/pixel/generatePixelsImage', {
-      method: 'GET',
+  const fetchData = async () => {
+    const data = await axios.get('https://2d15-102-46-146-22.ngrok-free.app/pixel/generatePixelsImage', {
+      responseType: 'arraybuffer',
       headers: {
         'Content-Type': 'application/json',
         "ngrok-skip-browser-warning": "true",
       },
-    }) 
-      .then(response => response.text()) 
-      .then(data => {
-        setSvgContent(data);  // Set the SVG content to state
-      })
-      .catch((error) => {
-        console.error('Error fetching SVG:', error);
-      });
-      dispatch(setLoading(false));
+    })
+    const blob = new Blob([data.data], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
+    const svg = new DOMParser().parseFromString(await (await fetch(url)).text(), 'image/svg+xml');
+    setSvgContent(svg.documentElement.outerHTML);
+  };
+  useEffect(() => {
+    fetchData();
+    dispatch(setLoading(false));
   }, []);
 
   return (
     <div className='w-100 border border-black'>
-      {/* <svg
+      <svg
         xmlns='http://www.w3.org/2000/svg'
-        href='https://2d15-102-46-146-22.ngrok-free.app/pixel/generatePixelsImage'
         viewBox='0 0 1000 1000'
         width='100%'
         height='100%'
         dangerouslySetInnerHTML={{ __html: svgContent! }}  // Set the SVG content
-        /> */}
-        <div dangerouslySetInnerHTML={{ __html: svgContent! }} />
+        />
     </div>
   );
 };
