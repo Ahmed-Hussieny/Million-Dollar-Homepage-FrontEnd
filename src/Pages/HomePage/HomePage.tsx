ad680e01a7f39fd4c1df95337701003106e1698b
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-12-30 12:27:05 +0200

    toast, env & small Documentation

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -4,54 +4,37 @@ import { setLoading } from '../../Store/globalSlice';
 import { getLogos } from '../../Store/LogosSlices';
 
 const HomePage: React.FC = () => {
-  const [svgContent, setSvgContent] = useState<string | null>(null); // To hold the SVG content as a string
+  const [svgContent, setSvgContent] = useState<string | null>(null);  // To hold the SVG content as a string
   const dispatch = useAppDispatch();
-
-  // This function will handle the SVG content and update the image paths
-  const updateImagePaths = (svg: string) => {
-    const imagePathPattern = /href="\/uploads\/([^"]+)"/g;
-    const updatedSvg = svg.replace(imagePathPattern, (_, p1) => {
-      return `href="https://2d15-102-46-146-22.ngrok-free.app/uploads/${p1}"`; // Update the path to the correct server URL
-    });
-    return updatedSvg;
-  };
-
   useEffect(() => {
     dispatch(setLoading(true));
     dispatch(getLogos()).unwrap();
-
     fetch('https://2d15-102-46-146-22.ngrok-free.app/pixel/generatePixelsImage', {
+      method: 'GET',
       headers: {
-        'ngrok-skip-browser-warning': 'true', // For ngrok tunnel if needed
+        'Content-Type': 'application/json',
+        "ngrok-skip-browser-warning": "true",
       },
-    })
-      .then(response => response.text())
+    }) 
+      .then(response => response.text()) 
       .then(data => {
-        const updatedSvg = updateImagePaths(data); // Update image paths
-        setSvgContent(updatedSvg); // Set the updated SVG content
+        setSvgContent(data);  // Set the SVG content to state
       })
       .catch((error) => {
         console.error('Error fetching SVG:', error);
-      })
-      .finally(() => {
-        dispatch(setLoading(false));
       });
-  }, [dispatch]);
+      dispatch(setLoading(false));
+  }, []);
 
   return (
-    <div className="w-100 border border-black">
-      {svgContent ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1000 1000"
-          width="100%"
-          height="100%"
-          dangerouslySetInnerHTML={{ __html: svgContent }} // Set the SVG content
+    <div className='w-100 border border-black'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 1000 1000'
+        width='100%'
+        height='100%'
+        dangerouslySetInnerHTML={{ __html: svgContent! }}  // Set the SVG content
         />
-      ) : (
-        <p>Loading...</p>
-      )}
-      
     </div>
   );
 };
